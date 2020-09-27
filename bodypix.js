5df05ee8bbd028818c73375ba5bea94f6858324a
Fidel Thomet <fidelio@NONPLACE.local>
2020-09-27 12:39:37 +0200

    init

---
+++ bodypix.js
@@ -0,0 +1,31 @@
+import bodyPix from '@tensorflow-models/body-pix';
+import fs from 'fs';
+import tfjs from '@tensorflow/tfjs-node';
+
+async function loadImage(path) {
+  const file = await fs.promises.readFile(path);
+
+  const image = await tfjs.node.decodeImage(file, 3);
+
+  return image;
+}
+
+async function main() {
+  const image = await loadImage('./img/test.jpg');
+
+  const net = await bodyPix.load({
+    architecture: "ResNet50",
+    quantBytes: 1,
+    outputStride: 16
+  });
+
+  const personSegmentation = await net.segmentPerson(image, {
+    internalResolution: "full",
+    segmentationThreshold: 0.5
+  });
+
+  console.log(personSegmentation)
+}
+
+
+main();
---
+++ p5.js
@@ -0,0 +1,19 @@
+import p5 from 'node-p5';
+ 
+function sketch(p) {
+    p.setup = () => {
+        let canvas = p.createCanvas(200, 200);
+        setTimeout(() => {
+            p.saveCanvas(canvas, 'myCanvas', 'jpg').then(filename => {
+                console.log(`saved the canvas as ${filename}`);
+            });
+        }, 100);
+        p.noLoop();
+    }
+    p.draw = () => {
+        p.background(255,0,255);
+        p.text('hello world!', 50, 100);
+    }
+}
+ 
+let p5Instance = p5.createSketch(sketch);
